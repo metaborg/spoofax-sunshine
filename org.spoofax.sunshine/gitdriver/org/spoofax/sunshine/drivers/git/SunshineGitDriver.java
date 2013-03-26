2639d7cb39394a8d11f63749b36a9474661ac4bc
Vlad Vergu <v.vergu@gmail.com>
2013-03-26 23:00:50 +0100

    Revision walking with proper cleanup, branch deletion, submodule updating.

--- org.spoofax.sunshine/gitdriver/org/spoofax/sunshine/drivers/git/SunshineGitDriver.java
+++ org.spoofax.sunshine/gitdriver/org/spoofax/sunshine/drivers/git/SunshineGitDriver.java
@@ -5,30 +5,19 @@ package org.spoofax.sunshine.drivers.git;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
-import org.eclipse.jgit.api.CheckoutCommand;
-import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode;
-import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRefNameException;
-import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.util.FS;
 import org.spoofax.sunshine.Environment;
 import org.spoofax.sunshine.LaunchConfiguration;
@@ -52,40 +41,63 @@ public class SunshineGitDriver extends SunshineMainDriver {
 		final File projectDir = Environment.INSTANCE().projectDir;
 		FileRepository repo;
 		try {
-			repo = (FileRepository) RepositoryCache.open(RepositoryCache.FileKey.lenient(projectDir, FS.DETECTED), true);
+			repo = (FileRepository) RepositoryCache
+					.open(RepositoryCache.FileKey.lenient(projectDir, FS.DETECTED), true);
 			git = new Git(repo);
 		} catch (IOException e) {
 			throw new RuntimeException("Git autopilot initialization failed", e);
 		}
-		
-		// FileRepositoryBuilder gitBuilder = new FileRepositoryBuilder();
-		// gitBuilder = gitBuilder.setGitDir(new File(projectDir,
-		// ".git")).readEnvironment().findGitDir();
-		// gitBuilder.setMustExist(true);
-		// try {
-		// git = new Git(gitBuilder.build());
-		// } catch (IOException e) {
-		// throw new RuntimeException("Could not find git repo", e);
-		// }
 	}
 
 	@Override
 	public void run() {
 		init();
 		List<RevCommit> commits = getCommits(true);
-		System.out.println(commits.size());
+		final int numCommits = commits.size();
+		RevCommit previous = null;
+		RevCommit current = null;
 		try {
-			final RevCommit target = commits.iterator().next();
-			System.out.println("Going back to commit: " + target.getId());
-			CheckoutCommand cmd = git.checkout();
-			cmd.setName("new-branch").setCreateBranch(true).setStartPoint(target).call();
-			System.out.println(cmd.getResult());
-			Runtime.getRuntime().exec("git clean -f -d", new String[0], git.getRepository().getDirectory());
+			for (int idx = 0; idx < numCommits; idx++) {
+				previous = current;
+				current = commits.get(idx);
+				assert current != null;
+				System.out.println("Checking out commit " + (idx + 1) + "/" + numCommits);
+				stepRevision(previous, current);
+				if (idx > 10)
+					break;
+			}
+			git.checkout().setName("master").call();
+			gitCleanVeryHard();
+			gitUpdateSubmodule();
+			gitDeleteBranch(current.getName());
 		} catch (GitAPIException e) {
 			throw new RuntimeException("Git autopilot crashed", e);
 		} catch (IOException e) {
 			throw new RuntimeException("Git autopilot crashed", e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Git autopilot crashed", e);
 		}
+	}
+
+	private void gitCleanVeryHard() throws InterruptedException, IOException {
+		Runtime.getRuntime().exec("git clean -f -f -d", new String[0], git.getRepository().getDirectory()).waitFor();
+	}
+
+	private void gitDeleteBranch(String branchname) throws GitAPIException {
+		git.branchDelete().setBranchNames(branchname).call();
+	}
+	
+	private void gitUpdateSubmodule() throws GitAPIException {
+		git.submoduleUpdate().call();
+	}
+
+	private void stepRevision(RevCommit from, RevCommit to) throws GitAPIException, IOException, InterruptedException {
+		git.checkout().setName(to.getName()).setCreateBranch(true).setStartPoint(to).call();
+		gitCleanVeryHard();
+		if (from != null) {
+			gitDeleteBranch(from.getName());
+		}
+		gitUpdateSubmodule();
 	}
 
 	private List<RevCommit> getCommits(boolean ascOrder) {
@@ -100,7 +112,7 @@ public class SunshineGitDriver extends SunshineMainDriver {
 		rw.setRevFilter(new FileExtensionRevFilter(LanguageService.INSTANCE().getSupportedExtens().iterator().next(),
 				git.getRepository()));
 		Iterator<RevCommit> iter = rw.iterator();
-		List<RevCommit> commits = new LinkedList<RevCommit>();
+		List<RevCommit> commits = new ArrayList<RevCommit>();
 		while (iter.hasNext()) {
 			commits.add(iter.next());
 		}
