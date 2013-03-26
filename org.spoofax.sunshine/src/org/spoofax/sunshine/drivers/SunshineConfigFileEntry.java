109e65852a191e9465c10c24e6a9f2d3a359c4c1
Vlad Vergu <v.vergu@gmail.com>
2013-03-26 12:24:28 +0100

    Dummy config file based entry

---
+++ org.spoofax.sunshine/src/org/spoofax/sunshine/drivers/SunshineConfigFileEntry.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.spoofax.sunshine.drivers;
+
+
+/**
+ * @author Vlad Vergu <v.a.vergu add tudelft.nl>
+ *
+ */
+public class SunshineConfigFileEntry {
+
+	
+	public static void main(String[] args) {
+		if(args.length != 1){
+			throw new IllegalArgumentException("Incorrect number of parameters");
+		}
+
+		throw new RuntimeException("Not implemented");
+	}
+}
