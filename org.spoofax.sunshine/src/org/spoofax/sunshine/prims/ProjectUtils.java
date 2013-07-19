949453320b2245bf13f42b299a68933f6147925c
Vlad Vergu <v.vergu@gmail.com>
2013-07-19 13:42:41 -0700

    Disable cleaning

--- org.spoofax.sunshine/src/org/spoofax/sunshine/prims/ProjectUtils.java
+++ org.spoofax.sunshine/src/org/spoofax/sunshine/prims/ProjectUtils.java
@@ -9,17 +9,8 @@ import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spoofax.interpreter.core.InterpreterException;
-import org.spoofax.interpreter.library.AbstractPrimitive;
-import org.spoofax.interpreter.library.IOperatorRegistry;
-import org.spoofax.interpreter.stratego.Strategy;
-import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.sunshine.CompilerException;
 import org.spoofax.sunshine.Environment;
-import org.spoofax.sunshine.services.LanguageService;
-import org.spoofax.sunshine.services.RuntimeService;
-import org.spoofax.sunshine.services.StrategoCallService;
-import org.strategoxt.HybridInterpreter;
 
 /**
  * @author vladvergu
@@ -30,36 +21,36 @@ public class ProjectUtils {
 	private static final Logger logger = LogManager.getLogger(ProjectUtils.class.getName());
 
 	public static void unloadIndex() {
-		logger.trace("Unloading index store for project {}",
-				Environment.INSTANCE().projectDir.getAbsolutePath());
-
-		HybridInterpreter runtime = RuntimeService.INSTANCE().getRuntime(
-				LanguageService.INSTANCE().getAnyLanguage());
-		IOperatorRegistry idxLib = runtime.getContext().getOperatorRegistry("INDEX");
-		AbstractPrimitive unloadIdxPrim = idxLib.get("LANG_index_unload");
-		try {
-			boolean unloadSuccess = unloadIdxPrim.call(
-					runtime.getContext(),
-					new Strategy[0],
-					new IStrategoTerm[] { runtime.getFactory().makeString(
-							Environment.INSTANCE().projectDir.getAbsolutePath()) });
-			if (!unloadSuccess) {
-				throw new CompilerException("Could not unload index");
-			}
-		} catch (InterpreterException intex) {
-			throw new CompilerException("Could not unload index", intex);
-		}
+		// logger.trace("Unloading index store for project {}",
+		// Environment.INSTANCE().projectDir.getAbsolutePath());
+		//
+		// HybridInterpreter runtime = RuntimeService.INSTANCE().getRuntime(
+		// LanguageService.INSTANCE().getAnyLanguage());
+		// IOperatorRegistry idxLib = runtime.getContext().getOperatorRegistry("INDEX");
+		// AbstractPrimitive unloadIdxPrim = idxLib.get("LANG_index_unload");
+		// try {
+		// boolean unloadSuccess = unloadIdxPrim.call(
+		// runtime.getContext(),
+		// new Strategy[0],
+		// new IStrategoTerm[] { runtime.getFactory().makeString(
+		// Environment.INSTANCE().projectDir.getAbsolutePath()) });
+		// if (!unloadSuccess) {
+		// throw new CompilerException("Could not unload index");
+		// }
+		// } catch (InterpreterException intex) {
+		// throw new CompilerException("Could not unload index", intex);
+		// }
 
 	}
 
 	public static void unloadTasks() {
-		logger.trace("Unloading task engine for project {}",
-				Environment.INSTANCE().projectDir.getAbsolutePath());
-		StrategoCallService.INSTANCE().callStratego(
-				LanguageService.INSTANCE().getAnyLanguage(),
-				"task-unload",
-				Environment.INSTANCE().termFactory.makeString(Environment.INSTANCE().projectDir
-						.getAbsolutePath()));
+		// logger.trace("Unloading task engine for project {}",
+		// Environment.INSTANCE().projectDir.getAbsolutePath());
+		// StrategoCallService.INSTANCE().callStratego(
+		// LanguageService.INSTANCE().getAnyLanguage(),
+		// "task-unload",
+		// Environment.INSTANCE().termFactory.makeString(Environment.INSTANCE().projectDir
+		// .getAbsolutePath()));
 	}
 
 	public static void cleanProject() {
