eedb76fc787cb4362138f496c2187e3b3f773c32
Gabriel Konat <gabrielkonat@gmail.com>
2015-02-03 16:08:33 +0100

    Dump log4j2 in favor of slf4j and logback for logging.

---
+++ org.spoofax.sunshine/src/main/java/org/metaborg/sunshine/pipeline/ISourceOne.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.metaborg.sunshine.pipeline;
+
+/**
+ * @author Vlad Vergu <v.a.vergu add tudelft.nl>
+ * 
+ */
+public interface ISourceOne<P> extends ISource<P> {
+
+    public void addSink(ISinkOne<P> sink);
+}
---
+++ org.spoofax.sunshine/src/main/java/org/metaborg/sunshine/statistics/BoxValidatable.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.metaborg.sunshine.statistics;
+
+/**
+ * @author Vlad Vergu <v.a.vergu add tudelft.nl>
+ * 
+ */
+public class BoxValidatable<T> implements IValidatable<T> {
+
+    public T value;
+
+    public BoxValidatable(T value) {
+        this.value = value;
+    }
+
+    @Override public boolean validate() {
+        return true;
+    }
+
+    @Override public T getValue() {
+        return value;
+    }
+
+}
